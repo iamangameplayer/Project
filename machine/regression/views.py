6d41d05285bfe935b1d346c116584a690bbfb29f
nagas <nagasaivignesh2005@gmail.com>
2026-05-02 18:19:15 +0530

    Initial commit

---
+++ machine/regression/views.py
@@ -0,0 +1,28 @@
+from django.views.decorators.csrf import csrf_exempt
+import joblib
+from django.shortcuts import render
+from django.http import JsonResponse
+
+model = joblib.load(open(r"C:\Users\nagas\PycharmProjects\Linear_Regression\regressor_bundled.joblib",'rb'))
+
+def home(request):
+    return render(request, 'index.html')
+
+@csrf_exempt
+def predict(request):
+    if request.method == 'POST':
+        features = [
+            float(request.POST.get('MedInc')),
+            float(request.POST.get('HouseAge')),
+            float(request.POST.get('AveRooms')),
+            float(request.POST.get('AveBedrms')),
+            float(request.POST.get('Population')),
+            float(request.POST.get('AveOccup')),
+            float(request.POST.get('Latitude')),
+            float(request.POST.get('Longitude')),
+        ]
+
+        prediction = model.predict([features])
+
+        return JsonResponse({'result': float(prediction[0])})
+
